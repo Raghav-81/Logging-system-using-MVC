d4edde887d6765af26d3a1c0972c5ce19630ee63
Raghav Pandit <71586087+Raghav-81@users.noreply.github.com>
2022-04-29 11:18:49 +0530

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-     // Assemble all the pieces of the MVC
-     Model m = new Model("", "", "");
-     View v = new View("MVC Log entry");
-     Controller c = new Controller(m, v);
-     c.initController();
-    }
-   }
